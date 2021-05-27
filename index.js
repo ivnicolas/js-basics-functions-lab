03713474851261cd338daba5b1700b5751769b12
ivnicolas <ikennanicolas@gmail.com>
2021-05-26 20:03:17 -0400

    commit tree 855839d444eb519288b9365849a7b6bc0b65f4b9

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(start){
+ return Math.abs(start - 42)
+}
+
+function distanceFromHqInFeet(start){
+    return (distanceFromHqInBlocks(start) * 264)
+}
+
+function distanceTravelledInFeet(start,end){
+    return (Math.abs(start-end) * 264)
+}
+
+function calculatesFarePrice(start, destination){
+    if (distanceTravelledInFeet(start,destination) <= 400){
+        return 0
+    }
+    else if (distanceTravelledInFeet(start,destination) > 400 && distanceTravelledInFeet(start,destination) < 2000 ){
+        return 2.56
+    }
+    else if (distanceTravelledInFeet(start,destination) > 2000 && distanceTravelledInFeet(start,destination) < 2500 ){
+        return 25
+    }
+    else {
+        return 'cannot travel that far'
+    }
+}
